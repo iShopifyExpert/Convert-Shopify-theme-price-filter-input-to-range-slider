375a8ac625f905765919142c2518c055d6b53b6b
Aaliyan Gul <aaliyan111@gmail.com>
2024-10-02 03:52:28 +0500

    Create update-in-assets-facets.js

---
+++ update-in-assets-facets.js
@@ -0,0 +1,3 @@
+#find this this.querySelectorAll('input') and replace with this.querySelectorAll('input[type="number"]')
+
+#find this const inputs = this.querySelectorAll('input'); and replace with const inputs = this.querySelectorAll('input[type="number"]');

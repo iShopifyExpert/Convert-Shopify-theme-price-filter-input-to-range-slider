282cbda3cfe004b153883c611eca171736b38723
Aaliyan Gul <aaliyan111@gmail.com>
2024-10-02 04:23:16 +0500

    Update price-range-slider.js

--- price-range-slider.js
+++ price-range-slider.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var inputFromLabel = document.querySelector('.price-range .price-range-label .field__label[for=From]');
+  var inputToLabel = document.querySelector('.price-range .price-range-label .field__label[for=To]');
   var inputFromRange = document.querySelector('input[type="range"][aria-label="From"]');
   var inputToRange = document.querySelector('input[type="range"][aria-label="To"]');
   var inputFromNumber = document.querySelector('.price-range__input input[name="filter.v.price.gte"]');
@@ -8,21 +10,24 @@ document.addEventListener('DOMContentLoaded', function () {
     if (parseInt(inputFromRange.value) > parseInt(inputToRange.value)) {
       inputFromRange.value = inputToRange.value;
       inputFromNumber.value = inputToRange.value;
+      inputFromLabel.innerText = inputToRange.value;
     }
   }
 
   // Update number input when the 'From' range changes
-  if (inputFromRange && inputFromNumber) {
+  if (inputFromRange && inputFromNumber && inputFromLabel) {
     inputFromRange.addEventListener('input', function () {
       inputFromNumber.value = this.value;
+      inputFromLabel.innerText = this.value;
       enforceRangeLogic();
     });
   }
 
   // Update number input when the 'To' range changes
-  if (inputToRange && inputToNumber) {
+  if (inputToRange && inputToNumber && inputToLabel) {
     inputToRange.addEventListener('input', function () {
       inputToNumber.value = this.value;
+      inputToLabel.innerText = this.value;
       enforceRangeLogic();
     });
   }
